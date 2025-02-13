f5a129675049555e28b1106ea287db35d62e83ac
Rafael Singer <rafaelsinger66@gmail.com>
2025-02-13 17:00:47 -0500

    fixed db issues

--- northwind/added_schema.sql
+++ northwind/added_schema.sql
@@ -6,3 +6,10 @@ CREATE TABLE IF NOT EXISTS Authentication (
 )
 
 CREATE INDEX IF NOT EXISTS idx_product_name ON Product(ProductName);
+
+CREATE TABLE IF NOT EXISTS Authentication (
+    UserID TEXT PRIMARY KEY,
+    Password TEXT,
+    SessionID TEXT,
+    FOREIGN KEY (UserID) REFERENCES Customer (Id)
+)
