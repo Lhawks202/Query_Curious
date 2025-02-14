30a88ecf4c6600caae21132a7fc82e57d7ffb7a7
Joshua Gorniak <joszek2004@gmail.com>
2025-02-14 11:06:32 -0500

    Add shopping cart functionality

--- northwind/__init__.py
+++ northwind/__init__.py
@@ -8,7 +8,7 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev', # need to store this in a .env file eventually
-        DATABASE=os.path.join(app.root_path, './northwind.db'),
+        DATABASE=os.path.join(app.root_path, './northwind.sqlite'),
     )
 
     if test_config is None:
@@ -31,7 +31,8 @@ def create_app(test_config=None):
     from . import db
     db.init_app(app)
 
-    from . import auth
+    from . import auth, cart
     app.register_blueprint(auth.bp)
+    app.register_blueprint(cart.bp)
 
     return app
