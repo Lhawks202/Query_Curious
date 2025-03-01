631da451006b1748b1939b4836a3175674e9e59a
lhawks202 <lhawks202@gmail.com>
2025-02-28 23:56:47 -0500

    Final test fixes

--- tests/test_checkout.py
+++ tests/test_checkout.py
@@ -14,18 +14,24 @@ def test_calc_cost():
     assert total_cost == 35.00
 
 
-def test_shipping_get(client):
-    response = client.get('/checkout/shipping/')
-    assert response.status_code == 200
-    response_text = response.data.decode('utf-8')
-    assert 'Shipping' in response_text
+def test_shipping_get(client, auth, app):
+    with app.app_context():
+        auth.register()
+        auth.login()
+        response = client.get('/checkout/shipping/')
+        assert response.status_code == 200
+        response_text = response.data.decode('utf-8')
+        assert 'Shipping Method' in response_text
 
 
-def test_shipping_post(client):
-    response = client.post('/checkout/shipping/', data={'shipping_method': 'United Package'})
-    assert response.status_code == 200
-    response_text = response.data.decode('utf-8')
-    assert 'Shipping' in response_text
+def test_shipping_post(client, auth, app):
+    with app.app_context():
+        auth.register()
+        auth.login()
+        response = client.post('/checkout/shipping/', data={'shipping_method': 'United Package'})
+        assert response.status_code == 200
+        response_text = response.data.decode('utf-8')
+        assert 'Shipping' in response_text
 
 
 def test_copy_to_orders(client, app, search, cart, auth):
@@ -69,6 +75,7 @@ def test_copy_to_orders(client, app, search, cart, auth):
         cart = db.execute("SELECT * FROM Shopping_Cart WHERE CartID = ?", (cart_id,)).fetchone()
         assert cart is None
 
+
 def test_delete_old_items(client, app, search, cart, auth):
     with app.app_context():
         db = get_db()
@@ -91,6 +98,7 @@ def test_delete_old_items(client, app, search, cart, auth):
         # Verify that old items were deleted
         cart_items = db.execute("SELECT * FROM Cart_Items WHERE CartID = ?", (cart_id,)).fetchall()
         assert len(cart_items) == 0
+
 
 def test_checkout_post(client, app, search, cart, auth):
     with app.app_context():
@@ -115,14 +123,86 @@ def test_checkout_post(client, app, search, cart, auth):
         assert 'Cart Total: $20.00' in response_text
         assert 'Shipping: United Package' in response_text
 
-def test_shipping_get(client):
-    response = client.get('/checkout/shipping/')
-    assert response.status_code == 200
-    response_text = response.data.decode('utf-8')
-    assert 'Choose Shipping Method' in response_text
 
-def test_shipping_post(client):
-    response = client.post('/checkout/shipping/', data={'shipping_method': 'United Package'})
-    assert response.status_code == 200
-    response_text = response.data.decode('utf-8')
-    assert 'Choose Shipping Method' in response_text
+def test_checkout_post_logged_in(client, app, search, cart, auth):
+    with app.app_context():
+        auth.register()
+        auth.login()
+        session_id = "testtesttesttesttesttesttesttest"
+        user_id = "testtestingauth"
+        supplier_id = search.insert_supplier()
+        category_id = search.insert_category()
+        product_id = search.insert_product(supplier_id=supplier_id, category_id=category_id)
+        cart_id = cart.insert_shopping_cart(session_id=session_id, user_id=user_id)
+        cart.insert_cart_items(cart_id, product_id, 2)
+
+        with client.session_transaction() as sess:
+            sess['session_id'] = session_id
+            sess['user_id'] = user_id
+
+        with app.test_request_context():
+            g.user = get_db().execute(
+                'SELECT * FROM Authentication WHERE UserID = ?', (user_id,)
+            ).fetchone()
+
+        response = client.post('/checkout/', data={'UserID': user_id, 'shipping': 'United Package'})
+        assert response.status_code == 200
+        response_text = response.data.decode('utf-8')
+        assert '<h1>Checkout</h1>' in response_text
+        assert 'Cart Total: $20.00' in response_text
+        assert 'Shipping: United Package' in response_text
+
+
+def test_checkout_post_not_logged_in(client, app, search, cart):
+    with app.app_context():
+        session_id = "testtesttesttesttesttesttesttest"
+        supplier_id = search.insert_supplier()
+        category_id = search.insert_category()
+        product_id = search.insert_product(supplier_id=supplier_id, category_id=category_id)
+        cart_id = cart.insert_shopping_cart(session_id=session_id)
+        cart.insert_cart_items(cart_id, product_id, 2)
+
+        with client.session_transaction() as sess:
+            sess['session_id'] = session_id
+
+        response = client.post('/checkout/', data={'shipping': 'United Package'})
+        assert response.status_code == 200  # Redirect to login
+
+
+def test_checkout_get_logged_in(client, app, search, cart, auth):
+    with app.app_context():
+        auth.register()
+        auth.login()
+        session_id = "testtesttesttesttesttesttesttest"
+        user_id = "testtestingauth"
+        supplier_id = search.insert_supplier()
+        category_id = search.insert_category()
+        product_id = search.insert_product(supplier_id=supplier_id, category_id=category_id)
+        cart_id = cart.insert_shopping_cart(session_id=session_id, user_id=user_id)
+        cart.insert_cart_items(cart_id, product_id, 2)
+
+        with client.session_transaction() as sess:
+            sess['session_id'] = session_id
+            sess['user_id'] = user_id
+
+        response = client.post('/checkout/', data={'shipping': 'United Package'})
+        assert response.status_code == 200
+        response_text = response.data.decode('utf-8')
+        assert '<h1>Checkout</h1>' in response_text
+        assert 'Cart Total: $20.00' in response_text
+        assert 'Shipping: United Package' in response_text
+
+
+def test_checkout_get_not_logged_in(client, app, search, cart):
+    with app.app_context():
+        session_id = "testtesttesttesttesttesttesttest"
+        supplier_id = search.insert_supplier()
+        category_id = search.insert_category()
+        product_id = search.insert_product(supplier_id=supplier_id, category_id=category_id)
+        cart_id = cart.insert_shopping_cart(session_id=session_id)
+        cart.insert_cart_items(cart_id, product_id, 2)
+
+        with client.session_transaction() as sess:
+            sess['session_id'] = session_id
+        response = client.post('/checkout/', data={'shipping': 'United Package'})
+        assert response.status_code == 200
