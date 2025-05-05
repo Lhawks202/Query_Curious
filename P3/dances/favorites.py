758c76d65648e0fd64cab80c4afe0afba268ab15
baekka1 <baekka@bc.edu>
2025-05-05 16:44:08 -0400

    fix sql errors

--- P3/dances/favorites.py
+++ P3/dances/favorites.py
@@ -14,14 +14,15 @@ def favorite_dances():
     if g.user is None:
         return render_template('favorites.html')
     favorite_dances = db.execute(
-                            'SELECT d.DanceName as dance_name, f.DateAdded as date_added, f.Rating as rating'
-                            'FROM Favorites f JOIN Dance d ON f.DanceId = d.ID'
-                            'WHERE UserId = ?', (g.user['UserID'],)).fetchall()
+                            '''SELECT d.DanceName as dance_name, f.DateAdded as date_added, f.Rating as rating
+                             FROM Favorites f JOIN Dance d ON f.DanceId = d.ID
+                             WHERE UserId = ?''',
+                            (g.user['UserID'],)).fetchall()
     
     dance_information = db.execute(
-            'SELECT d.ID, d.DanceName, d.Video, d.Date, d.Source, '
-            's.StepName, s.Sequence '
-            'FROM Favorites f JOIN Dance d ON f.DanceId = d.ID '
-            'JOIN Steps s ON s.DanceId = d.ID'
-            'WHERE UserId = ?', (g.user['UserID'],)).fetchall()
+            '''SELECT d.ID, d.DanceName, d.Video, d.Source, s.StepName
+             FROM Favorites f JOIN Dance d ON f.DanceId = d.ID 
+             JOIN Steps s ON s.DanceId = d.ID
+             WHERE UserId = ?''',
+            (g.user['UserID'],)).fetchall()
     return render_template('favorites.html', favorites=favorite_dances, dances=dance_information)
