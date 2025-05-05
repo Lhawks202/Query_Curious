87680ea32c6cdccf8774ca90002ec07a21e33a88
lhawks202 <lhawks202@gmail.com>
2025-05-05 15:42:27 -0400

    Added extra auth testing

--- P3/tests/test_auth.py
+++ P3/tests/test_auth.py
@@ -10,7 +10,6 @@ def test_register(client: FlaskClient, app: Flask, auth: Any) -> None:
     # Test invalid registration attempts
     response = auth.register(username='', password='testtestingauth')
     response_text = response.data.decode('utf-8')
-<<<<<<< HEAD
     assert 'User ID is required.' in response_text
     response = auth.register(username='', password='')
     response_text = response.data.decode('utf-8')
@@ -18,19 +17,6 @@ def test_register(client: FlaskClient, app: Flask, auth: Any) -> None:
     response = auth.register(username='test', password='')
     response_text = response.data.decode('utf-8')
     assert 'Password is required.' in response_text
-=======
-    
-    assert 'User ID is required.' in response_text
-
-    response = auth.register(username='', password='')
-    response_text = response.data.decode('utf-8')
-    assert 'User ID is required.' in response_text
-
-    response = auth.register(username='test', password='')
-    response_text = response.data.decode('utf-8')
-    assert 'Password is required.' in response_text
-    
->>>>>>> e4c6718 (Frameworked tests, waiting on main fixes)
     # Test successful registration and redirection
     response = auth.register()
     # Check if the user was added to the database
@@ -38,10 +24,6 @@ def test_register(client: FlaskClient, app: Flask, auth: Any) -> None:
         assert get_db().execute(
             "SELECT * FROM Authentication WHERE UserID = 'testtestingauth'",
         ).fetchone() is not None
-<<<<<<< HEAD
-=======
-    
->>>>>>> e4c6718 (Frameworked tests, waiting on main fixes)
     response = auth.register(username='testtestingauth2', password='testtestingauth2', next='/cart')
     # Check if the user was added to the database
     with app.app_context():
@@ -50,7 +32,7 @@ def test_register(client: FlaskClient, app: Flask, auth: Any) -> None:
         ).fetchone() is not None
     assert response.headers['Location'] == '/auth/login', "Post registration redirect location is incorrect."
 
-<<<<<<< HEAD
+
 def test_register_existing_user(client: FlaskClient, auth: Any) -> None:
     auth.register()
     response = auth.register()
@@ -60,23 +42,12 @@ def test_register_existing_user(client: FlaskClient, auth: Any) -> None:
         print(sess.get('_flashes', []))
     assert 'User already exists—try logging in!' in response_text
     
-=======
-
-def test_register_existing_user(auth: Any) -> None:
-    auth.register()
-    response = auth.register()
-    response_text = response.data.decode('utf-8')
-    assert 'Customer already exists—try logging in!' in response_text
-
-
->>>>>>> e4c6718 (Frameworked tests, waiting on main fixes)
 def test_register_strange_characters(auth: Any) -> None:
     response = auth.register(username='test_!@#$%^*&()`\'', password='test_!@#$%^*&()`\'')
     assert response.headers['Location'] == '/auth/login',  "Doesn't accept strange characters in username and password."
     response = auth.register(username='éñçøßΩ中あ😊€', password='éñçøßΩ中あ😊€')
     assert response.headers['Location'] == '/auth/login',  "Doesn't accept strange characters in username and password."
 
-<<<<<<< HEAD
 def test_sql_injection_drop_table_register(app: Flask, auth: Any) -> None:
     with app.app_context():
         # Attempt to register with SQL injection in the user_id to drop the User table
@@ -89,21 +60,6 @@ def test_sql_injection_drop_table_register(app: Flask, auth: Any) -> None:
             db.execute('SELECT 1 FROM User LIMIT 1')
         except Exception as e:
             assert False, f"Customer table was dropped: {e}"
-=======
-
-def test_sql_injection_drop_table_register(auth: Any) -> None:
-    # Attempt to register with SQL injection in the user_id to drop the User table
-    response = auth.register(username="'; DROP TABLE User; --", password='password')
-    assert response.status_code == 302
-
-    # Verify that the Customer table still exists
-    db = get_db()
-    try:
-        db.execute('SELECT 1 FROM User LIMIT 1')
-    except Exception as e:
-        assert False, f"Customer table was dropped: {e}"
-
->>>>>>> e4c6718 (Frameworked tests, waiting on main fixes)
 
 def test_login(client: FlaskClient, auth: Any) -> None:
     auth.register()
@@ -114,7 +70,7 @@ def test_login(client: FlaskClient, auth: Any) -> None:
     assert b'Incorrect password.' in response.data
     # Test successful login and redirection
     response = auth.login()
-<<<<<<< HEAD
+
     assert response.headers['Location'] == '/', "Post login redirect location is incorrect."
     with client:
         assert client.get('/').status_code == 200, "Internal Server Error on Login"
@@ -133,27 +89,6 @@ def test_sql_injection_drop_table_login(app: Flask, auth: Any) -> None:
             db.execute('SELECT 1 FROM Authentication LIMIT 1')
         except Exception as e:
             assert False, f"Authentication table was dropped: {e}"
-=======
-    assert response.headers['Location'] == '/cart/assign-user', "Post login redirect location is incorrect."
-    with client:
-        client.get('/')
-        assert session['user_id'] == 'testtestingauth'
-
-
-def test_sql_injection_drop_table_login(auth: Any) -> None:
-    auth.register()
-    # Attempt to login with SQL injection in the user_id to drop the Authentication table
-    response = auth.login(username="'; DROP TABLE Authentication; --", password='password')
-    assert response.status_code == 200
-    response_text = response.data.decode('utf-8')
-    assert 'Incorrect user id.' in response_text
-    db = get_db()
-    try:
-        db.execute('SELECT 1 FROM Authentication LIMIT 1')
-    except Exception as e:
-        assert False, f"Authentication table was dropped: {e}"
-
->>>>>>> e4c6718 (Frameworked tests, waiting on main fixes)
 
 def test_logout(client: FlaskClient, auth: Any) -> None:
     auth.register()
@@ -164,10 +99,7 @@ def test_logout(client: FlaskClient, auth: Any) -> None:
         auth.logout()
         assert 'test' not in session
 
-<<<<<<< HEAD
-=======
 
->>>>>>> e4c6718 (Frameworked tests, waiting on main fixes)
 def test_load_logged_in_user(client: FlaskClient, auth: Any) -> None:
     auth.register()
     auth.login()
